499262ad0716919b8ee9fa77858fc2a1c6d05e40
Reinaldo Haynes <rey.haynes@me.com>
2014-07-05 13:32:05 -0400

    Step #1: Initial Clean Project with Assets

---
+++ SwiftFlappy/GameScene.swift
@@ -0,0 +1,25 @@
+//
+//  GameScene.swift
+//  SwiftFlappy
+//
+//  Created by Reinaldo Haynes on 7/5/14.
+//  Copyright (c) 2014 Reinaldo Haynes. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
