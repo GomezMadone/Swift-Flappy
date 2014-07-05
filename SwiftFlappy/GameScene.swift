dd891c06dec2769e19730c8ae50d54186fdfea97
Reinaldo Haynes <rey.haynes@me.com>
2014-07-05 13:34:09 -0400

    Step #2: Adding Flappy Bird to the Scene

--- SwiftFlappy/GameScene.swift
+++ SwiftFlappy/GameScene.swift
@@ -9,9 +9,19 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var bird = SKSpriteNode()
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         
+        var birdTexture1 = SKTexture(imageNamed: "Bird1")
+        birdTexture1.filteringMode = SKTextureFilteringMode.Nearest
+        
+        bird = SKSpriteNode(texture:birdTexture1)
+        bird.position = CGPoint(x: self.frame.size.width / 2.8, y: CGRectGetMidY(self.frame))
+        
+        self.addChild(bird)
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
