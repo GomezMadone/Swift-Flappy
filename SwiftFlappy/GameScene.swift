10442eecedf037a5ad59a8b778ed87525b3f4d27
Reinaldo Haynes <rey.haynes@me.com>
2014-07-05 13:35:33 -0400

    Step #3: Make It Flap

--- SwiftFlappy/GameScene.swift
+++ SwiftFlappy/GameScene.swift
@@ -17,9 +17,15 @@ class GameScene: SKScene {
         
         var birdTexture1 = SKTexture(imageNamed: "Bird1")
         birdTexture1.filteringMode = SKTextureFilteringMode.Nearest
+        var birdTexture2 = SKTexture(imageNamed: "Bird2")
+        birdTexture2.filteringMode = SKTextureFilteringMode.Nearest
+        
+        var animation = SKAction.animateWithTextures([birdTexture1, birdTexture2], timePerFrame: 0.2)
+        var flap = SKAction.repeatActionForever(animation)
         
         bird = SKSpriteNode(texture:birdTexture1)
         bird.position = CGPoint(x: self.frame.size.width / 2.8, y: CGRectGetMidY(self.frame))
+        bird.runAction(flap)
         
         self.addChild(bird)
     }
